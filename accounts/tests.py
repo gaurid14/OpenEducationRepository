b5eae3e0dd7adb0b7326db53bbe709f30e8c06b1
UmmeAtiyaa <120266153+UmmeAtiyaa@users.noreply.github.com>
2025-10-25 21:29:39 +0530

    Delete accounts directory

--- accounts/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 7565647 (Initial project setup with Django, Postgres configs, and requirements.txt)
