8260e5a9f1b4592ff83b819ac2bfac11f1e6b6cd
Gauri Desai <108411074+gaurid14@users.noreply.github.com>
2025-11-10 20:17:55 +0530

    Update settings.py

--- oer/settings.py
+++ oer/settings.py
@@ -204,3 +204,11 @@ LOGGING = {
     },
 }
 
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "societyhub18@gmail.com"
+EMAIL_HOST_PASSWORD = "pqoq okex osoh riry"  # NOT your regular Gmail password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
